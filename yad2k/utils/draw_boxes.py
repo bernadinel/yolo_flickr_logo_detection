20010b043372f6e6728333e62b019c1228932e99
LBE <levi.bernadine@gmail.com>
2019-10-22 15:35:33 +0200

    adapt draw boxes arg path to ccloud drive

--- yad2k/utils/draw_boxes.py
+++ yad2k/utils/draw_boxes.py
@@ -2,7 +2,7 @@
 
 import colorsys
 import random
-
+import os
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
@@ -45,7 +45,7 @@ def draw_boxes(image, boxes, box_classes, class_names, scores=None):
     image = Image.fromarray(np.floor(image * 255 + 0.5).astype('uint8'))
 
     font = ImageFont.truetype(
-        font='font/FiraMono-Medium.otf',
+        font=os.path.join('/content/gdrive/My Drive/Gcolab/Data', 'FiraMono-Medium.otf'),
         size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
     thickness = (image.size[0] + image.size[1]) // 300
 
